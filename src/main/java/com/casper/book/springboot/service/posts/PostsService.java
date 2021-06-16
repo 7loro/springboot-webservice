fe48016b0093857c168676272fb617d98f16964a
Giung Nam <nowfeel88@gmail.com>
2021-06-16 15:42:24 +0900

    Posts 수정 api 추가 최신 문법 사용 BaseTimeEntity 추가

--- src/main/java/com/casper/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/casper/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.casper.book.springboot.service.posts;
 
+import com.casper.book.springboot.domain.posts.Posts;
 import com.casper.book.springboot.domain.posts.PostsRepository;
+import com.casper.book.springboot.web.dto.PostsResponseDto;
 import com.casper.book.springboot.web.dto.PostsSaveRequestDto;
+import com.casper.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,5 +17,23 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto) {
         return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
+        );
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        // 신기하게 repository 에 query 날리는 것이 없음. JPA 의 영속성 컨텍스트 때문.
+        return id;
+    }
+
+    @Transactional
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
+        );
+        return new PostsResponseDto(entity);
     }
 }
--- src/test/java/com/casper/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/casper/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -1,24 +1,25 @@
 package com.casper.book.springboot.domain.posts;
 
-import org.junit.After;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class PostsRepositoryTest {
 
     @Autowired
     PostsRepository postsRepository;
 
-    @After
+    @AfterEach
     public void cleanup() {
         postsRepository.deleteAll();
     }
@@ -42,5 +43,26 @@ public class PostsRepositoryTest {
         Posts posts = postsList.get(0);
         assertThat(posts.getTitle()).isEqualTo(title);
         assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2021,6,16,0,0,0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+
+        // then
+        Posts posts = postsList.get(0);
+        System.out.println(">>>>> CreateDate=" + posts.getCreatedDate() + ", modifiedDate=" + posts.getModifiedDate());
+
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
     }
 }
