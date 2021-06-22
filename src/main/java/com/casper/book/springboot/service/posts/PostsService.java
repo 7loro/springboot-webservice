87c97e9ad7b07557a5b396016e666550426b3f46
Giung Nam <nowfeel88@gmail.com>
2021-06-22 15:22:06 +0900

    게시글 수정, 삭제 구현

--- src/main/java/com/casper/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/casper/book/springboot/service/posts/PostsService.java
@@ -2,12 +2,17 @@ package com.casper.book.springboot.service.posts;
 
 import com.casper.book.springboot.domain.posts.Posts;
 import com.casper.book.springboot.domain.posts.PostsRepository;
+import com.casper.book.springboot.web.dto.PostsListResponseDto;
 import com.casper.book.springboot.web.dto.PostsResponseDto;
 import com.casper.book.springboot.web.dto.PostsSaveRequestDto;
 import com.casper.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +40,20 @@ public class PostsService {
                 () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
         );
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true) // 트랜잭션 범위는 유지하되 조회 기능만 남겨두어 조회 속도 개선
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(
+                () -> new IllegalArgumentException("해당 게시글이 없습니다. id=" + id)
+        );
+        postsRepository.delete(posts);
     }
 }
